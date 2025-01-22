7739763ace0e472821a5a19732e1e91a1249f3ec
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-22 22:22:22 +0300

    ...

--- Java/Prime Number Checker/PrimeNumberChecker.java
+++ Java/Prime Number Checker/PrimeNumberChecker.java
@@ -1,2 +1,24 @@
+import java.util.*;
 
-class 
+public class PrimeNumberChecker {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the whole number to check whether prime:");
+        int num = input.nextInt();
+
+        int count = 0;
+        for(int i = 2; i<num; i++){
+            if (num%i==0) {
+                count+=1;
+            }
+        }
+        if (count==1) {
+            System.out.println(num+" is a prime number");
+        }
+
+        
+
+    }
+}
