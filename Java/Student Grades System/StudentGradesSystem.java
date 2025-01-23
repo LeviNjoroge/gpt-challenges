bf18d57b918fff88cfd341a6b25a2492b2f85975
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-23 20:15:20 +0300

    finished task

--- Java/Student Grades System/StudentGradesSystem.java
+++ Java/Student Grades System/StudentGradesSystem.java
@@ -1,3 +1,27 @@
 // Build a program to input students' names and scores, 
 // and then calculate the average, highest, and lowest scores.
 
+import java.util.Scanner;
+public class StudentGradesSystem {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of the student: ");
+        String name = input.nextLine();
+        System.out.println("Enter the first score: ");
+        double score1 = input.nextDouble();
+        System.out.println("Enter the second score: ");
+        double score2 = input.nextDouble();
+        System.out.println("Enter the third score: ");
+        double score3 = input.nextDouble();
+        double average = (score1 + score2 + score3) / 3;
+        double highest = Math.max(score1, Math.max(score2, score3));
+        double lowest = Math.min(score1, Math.min(score2, score3));
+        System.out.println("The name of the student is: " + name);
+        System.out.println("The average score is: " + average);
+        System.out.println("The highest score is: " + highest);
+        System.out.println("The lowest score is: " + lowest);
+        input.close();
+    }
+}
