f90c5c57ed78bd0d058a03394b1d14196b3cac08
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-04 19:54:15 +0300

    working on task 13

--- Task 13/main.js
+++ Task 13/main.js
@@ -1,4 +1,6 @@
 
 document.querySelector("button").addEventListener('click',()=>{
-    
+    let date_and_time = Date();
+
+    document.getElementById("date_and_time").innerHTML = date_and_time;
 })
