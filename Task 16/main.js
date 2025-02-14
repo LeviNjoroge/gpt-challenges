bc1b23bf5500b4f47fca3abb9c8fc9b77c6a5591
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-14 23:10:11 +0300

    working on task 16

--- Task 16/main.js
+++ Task 16/main.js
@@ -2,11 +2,21 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
+    let result = document.querySelector("#result");
+    let add = document.querySelector("#add");
+    let subtract = document.querySelector("#subtract");
+    let multiply = document.querySelector("#multiply");
+    let divide = document.querySelector("#divide");
 
     add.onclick = function(){
         result.innerHTML = num1 + num2;
     }
+    subtract.onclick = function(){
+        result.innerHTML = num1 - num2;
+    }
+    
 
 
 });
