deb9578f3d88fb9bff5507f9cbcc99f5849da548
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-14 23:22:39 +0300

    working on task 16

--- Task 16/main.js
+++ Task 16/main.js
@@ -2,25 +2,30 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    let num1 = document.querySelector("#num1");
-    let num2 = document.querySelector("#num2");
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
     let result = document.querySelector("#result");
     let add = document.querySelector("#add");
     let subtract = document.querySelector("#subtract");
     let multiply = document.querySelector("#multiply");
     let divide = document.querySelector("#divide");
 
-    add.onclick = function(){
-        result.textContent = num1.value + num2.value;
-    }
-    subtract.onclick = function(){
-        result.textContent = num1 - num2;
-    }
-    multiply.onclick = function(){
-        result.textContent = num1 * num2;
-    }
-    divide.onclick = function(){
-        result.textContent = num1 / num2;
-    }
+    document.querySelector("button").addEventListener("click", function(){
 
+        let num1 = document.querySelector("#num1").value;
+        let num2 = document.querySelector("#num2").value;
+
+        add.onclick = function(){
+            result.textContent = num1 + num2;
+        }
+        subtract.onclick = function(){
+            result.textContent = num1 - num2;
+        }
+        multiply.onclick = function(){
+            result.textContent = num1 * num2;
+        }
+        divide.onclick = function(){
+            result.textContent = num1 / num2;
+        }
+    });
 });
