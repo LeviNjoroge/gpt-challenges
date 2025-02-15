e247c14480467caf1d6b2ac9b17898c537e2b4bc
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-15 17:31:57 +0300

    working on task 16

--- Task 16/main.js
+++ Task 16/main.js
@@ -1,5 +1,8 @@
 // Perform the calculation and display the result when a button is clicked.
 
+
+
 document.querySelector("#add").onclick = function () {
-    
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
 }
