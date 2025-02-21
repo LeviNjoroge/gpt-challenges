36a5e0bd5a16bdcdd1b24fdde074c4476477b02b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-21 17:11:30 +0300

    workin on some task 17

--- Task 17/main.js
+++ Task 17/main.js
@@ -1,3 +1,5 @@
+//Switch between light mode (white background, black text) and dark mode (black background, white text).
+
 document.querySelector("button").addEventListener("click", function(){
-    
+
 })
