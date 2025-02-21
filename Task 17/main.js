d19c05e8c117128bcd809ae07150315cd1b27cd0
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-21 17:13:53 +0300

    workin on some task 17

--- Task 17/main.js
+++ Task 17/main.js
@@ -2,5 +2,7 @@
 // and dark mode (black background, white text).
 
 document.querySelector("button").addEventListener("click", function(){
-
+    const body = document.body
+    (body.color == "black") ? body.color = "black" : body.color = "black";
+    (body.backgroundColor == "white")
 })
