d49cffb63c7c0d6e0fac4d03aa204aee1e14bbdd
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-23 21:21:40 +0300

    workin on some task 18

--- Task 18/main.js
+++ Task 18/main.js
@@ -1,3 +1,5 @@
+// Create an array of quotes.
+// Display a random quote from the array when the button is clicked. 
 
 const genQuote = document.querySelector("#genQuote");
 const quoteContent = document.querySelector("#qoute_content");
