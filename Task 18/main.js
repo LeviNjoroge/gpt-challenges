0172e21c5458f60dc14c237bf670fc1c03e41bc9
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-23 21:22:28 +0300

    workin on some task 18

--- Task 18/main.js
+++ Task 18/main.js
@@ -3,3 +3,17 @@
 
 const genQuote = document.querySelector("#genQuote");
 const quoteContent = document.querySelector("#qoute_content");
+
+const quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "The best way to predict the future is to create it. - Peter Drucker",
+    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+    "The only thing we have to fear is fear itself. - Franklin D. Roosevelt",
+    "The only thing necessary for the triumph of evil is for good men to do nothing. - Edmund Burke",
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+    "The only thing we have to fear is fear itself. - Franklin D. Roosevelt"
+]
+
