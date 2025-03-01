329d2118235ca5411fe1ef0ed5135553fab78a74
Levi Njoroge <Levynjoroge1@gmail.com>
2025-03-01 21:25:53 +0300

    working on task 20

--- Task 20/main.js
+++ Task 20/main.js
@@ -1,3 +1,8 @@
 // Count down from the entered number to zero.
 // Update the display every second. 
 
+<input type="number" name="num" id="num" placeholder="Enter number of seconds to countdown from">
+<button id="start">Start Timer</button>
+</form>
+<h1 id="display"></h1>
+
