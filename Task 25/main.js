71183aaa6bcd921a0348d6fca88e5e818e34bc08
Levi Njoroge <Levynjoroge1@gmail.com>
2025-03-30 21:46:58 +0300

    working on task 25

--- Task 25/main.js
+++ Task 25/main.js
@@ -1,6 +1,7 @@
 // Create a Digital Clock
 //     Display the current time and update it every second.
 
+const h1 = document.querySelector('h1');
 document.addEventListener('DOMContentLoaded', function() {
     
-})
+});
