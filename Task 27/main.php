483aff3ca7a2bb3efbf6b6cc2bd3906d13c1be53
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-17 18:14:15 +0300

    working on php

--- Task 27/main.php
+++ Task 27/main.php
@@ -1,7 +1,7 @@
 <!-- 
 1. Write a PHP script to find the largest of three numbers.
 2. Create an associative array of 5 countries and their capitals, and print them.
-
+3. Make a form that takes a user's name and displays a greeting.
 -->
 
 
