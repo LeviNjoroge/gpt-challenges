ed15601f57bb66024c93fb986affb57bf96b0ce1
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-18 19:33:53 +0300

    working on math using PHP

--- PHP Exercises/1.php
+++ PHP Exercises/1.php
@@ -1,3 +1,15 @@
 <!--
     1. Write a PHP script to find the largest of three numbers.
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Largest numbers</title>
+</head>
+<body>
+    
+</body>
+</html>
