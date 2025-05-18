e21dc2120c0d8518db9d11d4e82228f3579eec44
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-18 19:40:36 +0300

    working on math using PHP

--- PHP Exercises/1.php
+++ PHP Exercises/1.php
@@ -18,7 +18,11 @@
     </form>
 
     <?php
-        if(isset($_GET["num1"])&&isset($_GET["num1"])&&isset($_GET["num1"])){
+        if(isset($_GET["num1"])&&isset($_GET["num2"])&&isset($_GET["num3"])){
+            $num1 = $_GET["num1"];
+            $num2 = $_GET["num2"];
+            $num3 = $_GET["num3"];
+
             
         }
     ?>
