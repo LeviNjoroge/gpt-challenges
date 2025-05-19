547c7040ba65eab437ecb818533f5c3917caffc1
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-19 21:35:53 +0300

    working on math using PHP

--- PHP Exercises/2.php
+++ PHP Exercises/2.php
@@ -1,3 +1,15 @@
 <!--
     2. Create an associative array of 5 countries and their capitals, and print them.
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
