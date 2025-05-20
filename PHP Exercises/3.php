6a7c5e009ef97b0bcf779160d1396ec17fb0f363
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-20 22:42:37 +0300

    working on math using PHP

--- PHP Exercises/3.php
+++ PHP Exercises/3.php
@@ -1,3 +1,15 @@
 <!--
     3. Make a form that takes a user's name and displays a greeting.
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
