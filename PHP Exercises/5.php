b37e22ea2c1ab6ddcecffffe2dc6b597d7759bfe
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-23 18:20:56 +0300

    jus practicing my PHP using some gpt questions

--- PHP Exercises/5.php
+++ PHP Exercises/5.php
@@ -5,7 +5,7 @@
 <?php
     if(isset($_POST["username"])&&isset($_POST["password"])){
         $username = $_POST["username"];
-        $password = $_POST[""]
+        $password = $_POST["password"];
     }
 ?>
 
