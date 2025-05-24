a28de4a4c1e215f2ea98e09b3028b135920db78d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-24 18:22:04 +0300

    jus practicing my PHP using some gpt questions

--- PHP Exercises/6.php
+++ PHP Exercises/6.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Reading a text file using PHP</title>
 </head>
 <body>
     
