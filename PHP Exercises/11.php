fa85994f605250b5d0b7a52b6ddf53d6e2c891b4
Levi Njoroge <Levynjoroge1@gmail.com>
2025-06-04 18:07:53 +0300

    Working on PHP

--- PHP Exercises/11.php
+++ PHP Exercises/11.php
@@ -2,6 +2,8 @@
     // 11. Create a class for a "Bank Account" with deposit and withdraw methods.
 
     class bankAccount{
+        // should have attributes and methods
+        
         
     }
 ?>
